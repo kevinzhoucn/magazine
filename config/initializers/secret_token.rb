2a94c7d55e8ac916be55a24fd89d9ce972e6748e
kevinzhoucn <zzpf@163.com>
2012-05-19 05:25:26 +0800

    magazine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magazine::Application.config.secret_token = '1a789b870846b44b4568f8ade21db1aeb577cc5a9651d2338ed4a6548bbca010f4b2700a18c929c24a48cc4711975fb0bb8c21227ae0bc29747066b440101a40'
