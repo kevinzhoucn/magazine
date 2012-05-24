0bd2e746ad4ea3a02216696bc7fd4d0d0d912248
kevin <zzpf@163.com>
2012-05-24 17:29:36 +0800

    Adding files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private
+    def current_cart
+      Cart.find(session[:card_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:card_id] = cart.id
+      cart
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Magazine::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
+  get "store/index"
+
   resources :products
+  
+  root :to => 'store#index', :as => 'store'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
